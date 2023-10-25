6f76f8a59bfd7ea08435fdc713bc38805a7509c9
Mohosin212601~ <mohosin2126@gmail.com>
2023-10-26 00:29:17 +0800

    changes the article of our brand

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
          <div className="mt-16 ">
             <div>
                 <h1 className="text-3xl text-center font-serif font-semibold">Our Brand</h1>
-                <p className="text-center font-sans">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae aliquid placeat unde veniam molestiae vel animi hic inventore! Fugit, corporis!</p>
+                <p className="text-center font-sans">Harnessing the power of creativity and a forward-thinking approach, our brand is heading towards a destination where breakthroughs and success converge to create a legacy of distinction</p>
             </div>
          <div className="grid md:grid-cols-3 gap-5 mt-14">
           {
